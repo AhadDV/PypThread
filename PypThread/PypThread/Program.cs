f38136fbac73a85e6bed86f54dffa59655b65c77
Ahadvt <ahadvt@code.edu.az>
2022-08-30 01:16:12 +0400

    AddedTime

--- PypThread/PypThread/Program.cs
+++ PypThread/PypThread/Program.cs
@@ -1,21 +1,26 @@
-﻿HttpClient HttpClient = new();
+﻿using System.Diagnostics;
+
+HttpClient HttpClient = new();
 var list = new List<Task<string>>
 {
-    HttpClient.GetStringAsync("https://www.youtube.com/watch?v=GQZpGgwHGj4"),
-    HttpClient.GetStringAsync("https://www.facebook.com/ehed.tagyev.14"),
-    HttpClient.GetStringAsync("https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/"),
-    HttpClient.GetStringAsync("https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/")
+    HttpClient.GetStringAsync("https://ahaddv.github.io/portfolio/"),
+    HttpClient.GetStringAsync("https://beacon.az/"),
+    HttpClient.GetStringAsync("http://birustmedia.az/"),
+    HttpClient.GetStringAsync("https://ozonaptek.az/bio"),
 };
+var time =new Stopwatch();
+time.Start();
 await PrintHtmlLength(list);
+Console.WriteLine(time.ElapsedMilliseconds+" "+" ms");
 
-
+//ilk hansini elde etdise onu yazdirir ve buanda digerleri yuklenmeye davam edir
 
 async Task PrintHtmlLength(List<Task<string>> list)
 {
     while (list.Count > 0)
     {
         var result = await Task.WhenAny(list);
-        Console.WriteLine($"{result.Result.Length}");
+        Console.WriteLine($"{result.Result.Length}-length");
         list.Remove(result);
     }
 }
