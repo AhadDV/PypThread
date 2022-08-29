ebf844c99d9d99f4618e909a111e5451599bb3de
Ahadvt <ahadvt@code.edu.az>
2022-08-30 01:06:38 +0400

    done

---
+++ PypThread/PypThread/Program.cs
@@ -0,0 +1,34 @@
+﻿HttpClient HttpClient = new();
+var list = new List<Task<string>>
+{
+    HttpClient.GetStringAsync("https://www.youtube.com/watch?v=GQZpGgwHGj4"),
+    HttpClient.GetStringAsync("https://www.facebook.com/ehed.tagyev.14"),
+    HttpClient.GetStringAsync("https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/"),
+    HttpClient.GetStringAsync("https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/")
+};
+await PrintHtmlLength(list);
+
+
+
+async Task PrintHtmlLength(List<Task<string>> list)
+{
+    while (list.Count > 0)
+    {
+        var result = await Task.WhenAny(list);
+        Console.WriteLine($"{result.Result.Length}");
+        list.Remove(result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
